f43ce996062e230220f8848dc40a40d10e86da82
mysoyul <vega2k_@naver.com>
2024-01-25 16:18:08 +0900

    userinfo entity add

---
+++ src/main/java/com/boot3/myrestapi/security/userinfo/entity/UserInfo.java
@@ -0,0 +1,26 @@
+package com.boot3.myrestapi.security.userinfo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    private String roles;
+}
