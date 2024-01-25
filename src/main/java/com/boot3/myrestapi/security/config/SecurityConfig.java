4e031c23deaa2c7c7088b31a7efcd1c34e2d0f8e
mysoyul <vega2k_@naver.com>
2024-01-26 01:43:48 +0900

    springdoc-openapi dependency add

--- src/main/java/com/boot3/myrestapi/security/config/SecurityConfig.java
+++ src/main/java/com/boot3/myrestapi/security/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -98,5 +99,11 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
             throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/v3/api-docs/**","/swagger-ui.html", "/swagger-ui/**");
     }
 }
