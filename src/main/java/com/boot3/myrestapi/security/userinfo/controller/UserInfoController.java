d52e211987ceaec2b35aee2157ef16d5400b562d
mysoyul <vega2k_@naver.com>
2024-01-25 17:43:05 +0900

    login add

--- src/main/java/com/boot3/myrestapi/security/userinfo/controller/UserInfoController.java
+++ src/main/java/com/boot3/myrestapi/security/userinfo/controller/UserInfoController.java
@@ -1,8 +1,14 @@
 package com.boot3.myrestapi.security.userinfo.controller;
 
+import com.boot3.myrestapi.security.dto.AuthRequest;
+import com.boot3.myrestapi.security.jwt.JwtService;
 import com.boot3.myrestapi.security.userinfo.entity.UserInfo;
 import com.boot3.myrestapi.security.userinfo.repository.UserInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,11 +20,30 @@ public class UserInfoController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtService jwtService;
+
     @PostMapping("/new")
     public String addNewUser(@RequestBody UserInfo userInfo){
         userInfo.setPassword(passwordEncoder.encode(userInfo.getPassword()));
         UserInfo savedUserInfo = repository.save(userInfo);
         return savedUserInfo.getName() + " user added!!";
+    }
+
+    @PostMapping("/login")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authRequest.getEmail(),
+                        authRequest.getPassword()
+                ));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getEmail());
+        } else {
+            throw new UsernameNotFoundException("invalid user request !");
+        }
     }
 
     @GetMapping("/welcome")
